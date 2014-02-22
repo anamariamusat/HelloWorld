609984706b956c4ec03f83785539f0eb2364b6f8
lucianmusat <amibadesign@gmail.com>
2014-02-22 15:32:38 +0200

    1.0
    Hello World App

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,14 @@
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	printf("Tech Savy Geeks say: Hello World!");
+	std::cin.ignore();
+	return 0;
+}
+
